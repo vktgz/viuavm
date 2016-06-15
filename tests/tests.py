3a4bca42d54e8554b08c03eb2892e559f70250b3
Marek Marecki <marekjm@ozro.pw>
2016-06-15 23:03:58 +0200

    Run the test for negative indexes

--- tests/tests.py
+++ tests/tests.py
@@ -1103,6 +1103,14 @@ class AssemblerErrorTests(unittest.TestCase):
         self.assertEqual(1, exit_code)
         self.assertEqual("./sample/asm/errors/main_function_is_not_defined.asm: error: main function is not defined", output.strip())
 
+    def testNegativeRegisterIndex(self):
+        name = 'negative_register_index.asm'
+        assembly_path = os.path.join(self.PATH, name)
+        compiled_path = os.path.join(COMPILED_SAMPLES_PATH, '{0}_{1}.bin'.format(self.PATH[2:].replace('/', '_'), name))
+        output, error, exit_code = assemble(assembly_path, compiled_path, okcodes=(1,0))
+        self.assertEqual(1, exit_code)
+        self.assertEqual("./sample/asm/errors/negative_register_index.asm: error in function main/0 (at instruction 4): invalid register index (cannot be negative): -1", output.strip())
+
 
 class AssemblerErrorRejectingDuplicateSymbolsTests(unittest.TestCase):
     PATH = './sample/asm/errors/single_definition_rule'
